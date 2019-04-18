17f13707093038306184dc43ecc665c51cca136f
Murat Can Tanrıverdi <48529267+murattanriverdi445@users.noreply.github.com>
2019-04-18 17:55:02 +0300

    Update Main.java

--- java/Tortoise_and_Hare/Main.java
+++ java/Tortoise_and_Hare/Main.java
@@ -1,6 +1,21 @@
-import java.lang.reflect.Array;
+        /*Tosbağa ve Tavşan Yarışı 
+        70 kareden oluşan bir alanda yarış yapılmaktadır.İlk 70. alana ulaşan yarışı kazanır(dizi kullandığından 0-69 arası)
+        
+        Rastgele 1-10 arasında  üretilen 2 sayı var .
+        Tavşan için;
+        1 veya 2 gelirse => Uyku hiç hareket etme
+        3 veya 4 gelirse => 9 kare ileri hareket et
+        5 gelirse => 12 kare geri hareket et
+        6-7-8 gelirse => 1 kare ileri hareket et
+        9 veya 10 gelirse => 2 kare geri hareket et
+        
+        Tosbağa İçin;
+        1-2-3-4-5 gelirse => 3 kare ileri hareket et
+        6 veya 7 gelirse => 6 kare geri hareket et
+        8-9-10 gelirse => 1 kare ileri hareket et.
+                    
+*/
 import java.security.SecureRandom;
-import java.util.Arrays;
 
 public class Main {
 
@@ -22,139 +37,102 @@ public class Main {
 
         while(true){
 
-        int kapSayi=rast.nextInt(10)+1;
-        int tavSayi=rast.nextInt(10)+1;
+            int kapSayi=rast.nextInt(10)+1;
+            int tavSayi=rast.nextInt(10)+1;
+
+            System.out.println("\n"+kapSayi+" kaplumbaga konum= "+kaplumbaga1.getKonum());
+            System.out.println(tavSayi+"  tavşan konum= "+tavsan1.getKonum()+"\n");
 
 
 
-        if(kapSayi>=1 && kapSayi<=5){
-            kareler[kaplumbaga1.getKonum()]="-";
-            kaplumbaga1.ileriGit(3);
-            kareler[kaplumbaga1.getKonum()]="K";
+             if(tavSayi==3 || tavSayi==4){
+                kareler[tavsan1.getKonum()]="-";
+                tavsan1.ileriGit(9);
+                kareler[tavsan1.getKonum()]="T";
 
+
+            }
+            else if(tavSayi==5){
+                kareler[tavsan1.getKonum()]="-";
+                tavsan1.geriGit(5);
+                kareler[tavsan1.getKonum()]="T";
+
+
+
+            }
+            else if(tavSayi>=6 && tavSayi<=8){
+                kareler[tavsan1.getKonum()]="-";
+                tavsan1.ileriGit(1);
+                kareler[tavsan1.getKonum()]="T";
+
+
+            }
+            else{
+                kareler[tavsan1.getKonum()]="-";
+                tavsan1.geriGit(2);
+                kareler[tavsan1.getKonum()]="T";
+
+
+            }
+
+
+            if(kapSayi>=1 && kapSayi<=5  ){
+                kareler[kaplumbaga1.getKonum()]="-";
+                kaplumbaga1.ileriGit(3);
+                kareler[kaplumbaga1.getKonum()]="K";
+
+            }
+            else if(kapSayi==6 || kapSayi==7){
+                kareler[kaplumbaga1.getKonum()]="-";
+                kaplumbaga1.geriGit(6);
+                kareler[kaplumbaga1.getKonum()]="K";
+
+            }
+            else{
+                kareler[kaplumbaga1.getKonum()]="-";
+                kaplumbaga1.ileriGit(1);
+                kareler[kaplumbaga1.getKonum()]="K";
+
+            }
+
+
+
+
+            if(tavsan1.getKonum()<69 && kaplumbaga1.getKonum()<69){
+                kareler[tavsan1.getKonum()]="T";
+                kareler[kaplumbaga1.getKonum()]="K";
+
+            }
+            if(tavsan1.getKonum()==kaplumbaga1.getKonum()){
+                kareler[tavsan1.getKonum()]="T/K";
+            }
             for(int j=0;j<kareler.length;j++){
                 System.out.print(kareler[j]+" ");
             }
             System.out.println();
+            System.out.println(" kaplumbaga konum= "+kaplumbaga1.getKonum());
+            System.out.println("  tavşan konum= "+tavsan1.getKonum());
+            if(tavsan1.getKonum()==69 || kaplumbaga1.getKonum()==69){
+                if(tavsan1.getKonum()==69){
+                    System.out.println(" HARE   WINS!!! YAY!! ");
 
+                    break;
+                }
+                else if(kaplumbaga1.getKonum()==69){
+                    System.out.println(" TORTOISE  WINS!!! YAY!! ");
+                    break;
+                }
+                else if(kaplumbaga1.getKonum()==69 && tavsan1.getKonum()==69){
+                    System.out.printf("It’s a tie between %s and  %s", tavsan1.getAdi(),kaplumbaga1.getAdi());
 
+                    break;
+                }
 
-
-
-        }
-        else if(kapSayi==6 || kapSayi==7){
-            kareler[kaplumbaga1.getKonum()]="-";
-            kaplumbaga1.geriGit(6);
-            kareler[kaplumbaga1.getKonum()]="K";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-        }
-        else{
-            kareler[kaplumbaga1.getKonum()]="-";
-            kaplumbaga1.ileriGit(1);
-            kareler[kaplumbaga1.getKonum()]="K";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-        }
-
-        if(tavSayi==1 || tavSayi==2 ){
-
-        }
-        else if(tavSayi==3 || tavSayi==4){
-            kareler[tavsan1.getKonum()]="-";
-            tavsan1.ileriGit(9);
-            kareler[tavsan1.getKonum()]="T";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-        }
-        else if(tavSayi==5){
-            kareler[tavsan1.getKonum()]="-";
-            tavsan1.geriGit(5);
-            kareler[tavsan1.getKonum()]="T";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-
-        }
-        else if(tavSayi>=6 && tavSayi<=8){
-            kareler[tavsan1.getKonum()]="-";
-            tavsan1.ileriGit(1);
-            kareler[tavsan1.getKonum()]="T";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-        }
-        else{
-            kareler[tavsan1.getKonum()]="-";
-            tavsan1.geriGit(2);
-            kareler[tavsan1.getKonum()]="T";
-            for(int j=0;j<kareler.length;j++){
-                System.out.print(kareler[j]+" ");
-            }
-            System.out.println();
-
-        }
-
-
-        if(tavsan1.getKonum()<69 && kaplumbaga1.getKonum()<69){
-            kareler[tavsan1.getKonum()]="T";
-            kareler[kaplumbaga1.getKonum()]="K";
-
-        }
-        else if(tavsan1.getKonum()==kaplumbaga1.getKonum()){
-
-            kareler[tavsan1.getKonum()]="T/K";
-        }
-        else if(tavsan1.getKonum()==69 || kaplumbaga1.getKonum()==69){
-            if(tavsan1.getKonum()==69){
-                System.out.println(" HARE   WINS!!! YAY!! ");
-
-                break;
-            }
-            else if(kaplumbaga1.getKonum()==69){
-                System.out.println(" TORTOISE  WINS!!! YAY!! ");
-                break;
-            }
-            else if(kaplumbaga1.getKonum()==69 && tavsan1.getKonum()==69){
-                System.out.printf("It’s a tie between %s and  %s", tavsan1.getAdi(),kaplumbaga1.getAdi());
-
-                break;
             }
 
+            System.out.println("******************************************************************************************************\n");
+
         }
-
-
-        }
-
-        for(int j=0;j<kareler.length;j++){
-            System.out.print(kareler[j]+" ");
-        }
-
-        System.out.println("\n"+tavsan1.getKonum()+" tavşan");
-        System.out.println(kaplumbaga1.getKonum()+" kaplumbağa");
-
-
-
-
-
-
-
-
-
-
 
 
     }
